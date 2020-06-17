330376fa2b471133df7f6e7438024d6dd28173f4
Allegra Pusateri <allegrapusateri@gmail.com>
2020-06-17 15:18:53 -0500

    Added background image to about page

--- src/routes/About/About.js
+++ src/routes/About/About.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 ////////////////////////////////////////////////////////////////////////////////
 import './About.css';
+import background from './about-background.png'
 ////////////////////////////////////////////////////////////////////////////////
 
 class About extends Component {
@@ -12,7 +13,11 @@ class About extends Component {
                     <header className='line_height'>
                         <h1>I'm Allegra,</h1>
                         <p>a web developer and copywriter.</p>
+                        <img src={background} alt="Chicago skyline"/>
                     </header>
+                    <div>
+                        <p>TBD</p>
+                    </div>
                 </main>
             </>
         )
